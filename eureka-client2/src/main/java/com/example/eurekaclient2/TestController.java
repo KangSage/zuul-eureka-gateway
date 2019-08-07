1a14fc0c8610364d4a13ab142cdd3ccce34aa16d
ksage <ksage@zinnaworks.com>
2019-08-07 18:37:17 +0900

    Initial commit

---
+++ eureka-client2/src/main/java/com/example/eurekaclient2/TestController.java
@@ -0,0 +1,27 @@
+package com.example.eurekaclient2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @Value("${server.port}")
+    private String portNumber;
+
+    @GetMapping("/test")
+    public ResponseEntity<Map<String, Object>> testPage() {
+        log.info("portNumber: {}", portNumber);
+        Map<String, Object> result = new HashMap<>();
+        result.put("port", portNumber);
+        return ResponseEntity.ok(result);
+    }
+
+}
